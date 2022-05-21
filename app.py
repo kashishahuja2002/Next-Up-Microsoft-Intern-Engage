507c2419a13b961c5a428649042a6964bf6858dc
Kashish Ahuja <kashish.ahuja-IT-2K19-24@iips.edu.in>
2022-05-22 02:59:42 +0530

    updated backend

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 
-import json
 import pickle
 import numpy as np
 import pandas as pd
 import requests
 import flask
 import ast
-from flask import Flask, render_template
-# from flask_cors import CORS, cross_origin
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
-dataset = pickle.load(open('dataset.pkl', 'rb'))
+dataset = pickle.load(open('./model/data/dataset.pkl', 'rb'))
 movies = pd.DataFrame(dataset)
 
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+similarity = pickle.load(open('./model/data/similarity.pkl', 'rb'))
+popular = pickle.load(open('./model/data/popular.pkl', 'rb'))
 
 movie_names = movies['title'].values
 
@@ -24,20 +24,17 @@ def fetchTrailer(movie_id):
         'https://api.themoviedb.org/3/movie/{}/videos?api_key=65669b357e1045d543ba072f7f533bce&language=en-US'.format(
             movie_id))
     data = response.json()
-    print(data)
     for vid in data['results']:
-        if (vid['name'] == "Official Trailer"):
+        if vid['name'].find("Trailer") != -1:
             return vid['key']
 
 
 def fetchPoster(movie_id):
-    print(movie_id)
     response = requests.get(
         'https://api.themoviedb.org/3/movie/{}?api_key=65669b357e1045d543ba072f7f533bce&language=en-US'.format(
             movie_id))
     data = response.json()
-    print(str(data['poster_path']))
-    return "https://image.tmdb.org/t/p/w185/" + str(data['poster_path'])
+    return "https://image.tmdb.org/t/p/w185" + str(data['poster_path'])
 
 @app.route('/movie/<movie_name>')
 def movie(movie_name):
@@ -60,39 +57,23 @@ def movie(movie_name):
     movie_id = movies.iloc[movie_idx].movie_id
     recommendations, posters = recommend(selected_movie)
     trailer_key = fetchTrailer(movie_id)
+    movie_poster = fetchPoster(movie_id)
 
-    return render_template("movie.html", movie_names=movie_names, movies=movies, movie_idx=movie_idx, recommendations=recommendations, posters=posters, trailer_key=trailer_key)
+    return render_template("movie.html", movie_names=movie_names, movies=movies, movie_idx=movie_idx, recommendations=recommendations, posters=posters, trailer_key=trailer_key, movie_poster=movie_poster)
 
-@app.route('/recommendations')
-def recommendations():
-    def byYear(year):
-        counter = 0
-        L = []
-        for row in movies.iterrows():
-            if row[1].release_date == year:
-                if counter != 6:
-                    L.append(row[1].title)
-                    counter = counter + 1
-                else:
+
+def byGenre(genre):
+    counter = 0
+    genre_movies = []
+    for row in popular.iterrows():
+        if counter != 6:
+            for gen in row[1].genres:
+                if gen == genre:
+                    genre_movies.append(row[1].title)
+                    counter += 1
                     break
-        return L
-
-    def byGenre(genre):
-        counter = 0
-        L = []
-        for row in movies.iterrows():
-            if counter != 6:
-                for gen in row[1].genres:
-                    if gen == genre:
-                        L.append(row[1].title)
-                        counter += 1
-                        break
-            else:
-                break
-        return L
-
-    genre_movies = byGenre("Comedy")
-    year_movies = byYear("2016")
+        else:
+            break
 
     genre_posters = []
     for i in genre_movies:
@@ -100,12 +81,49 @@ def recommendations():
         movie_id = movies.iloc[movie_index].movie_id
         genre_posters.append(fetchPoster(movie_id))
 
+    return genre_movies, genre_posters
+
+def byYear(year):
+    counter = 0
+    year_movies = []
+    for row in popular.iterrows():
+        if row[1].release_date == year:
+            if counter != 6:
+                year_movies.append(row[1].title)
+                counter = counter + 1
+            else:
+                break
+
     year_posters = []
     for i in year_movies:
         movie_index = movies[movies['title'] == i].index[0]
         movie_id = movies.iloc[movie_index].movie_id
         year_posters.append(fetchPoster(movie_id))
 
+    return year_movies, year_posters
+
+@app.route('/getByGenre', methods=['GET','POST'])
+def getByGenre():
+    genre = request.form["genre"]
+    genre_movies, genre_poster = byGenre(genre)
+    response = jsonify(
+        {"genre_movies": genre_movies}, {"genre_posters": genre_poster}
+    )
+    return response
+
+@app.route('/getByYear', methods=['GET','POST'])
+def getByYear():
+    year = request.form["year"]
+    year_movies, year_poster = byYear(year)
+    response = jsonify(
+        {"year_movies": year_movies}, {"year_posters": year_poster}
+    )
+    return response
+
+@app.route('/recommendations')
+def recommendations():
+    genre_movies, genre_posters = byGenre("Animation")
+    year_movies, year_posters = byYear("2016")
     return render_template("recommendations.html", movie_names=movie_names, genre_movies=genre_movies, year_movies=year_movies, genre_posters=genre_posters, year_posters=year_posters)
 
 if __name__ == "__main__":
