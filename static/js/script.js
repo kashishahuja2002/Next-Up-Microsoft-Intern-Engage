c5b2a0c8d397fcc71c6437521fc4135cc369306a
Kashish Ahuja <kashish.ahuja-IT-2K19-24@iips.edu.in>
2022-05-22 02:59:25 +0530

    updated front-end

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,18 @@ $(document).ready(function() {
     $('.select2').select2();
 });
 
+// Search select
+function movieSelect()
+{
+    let selection = document.getElementById('movieSelect');
+    let selectedMovie = selection.options[selection.selectedIndex].text;
+
+    var url = "http://127.0.0.1:5000/movie/"+selectedMovie;
+    window.location = url;
+}
+
+
+
 // Recommendations Page
 
 // Movie Slider carousel
@@ -13,12 +25,40 @@ $('.carousel').carousel({
     keyboard: true
 })
 
-// Movie Page
-function movieSelect()
+// select by genre
+function genreSelected()
 {
-    let selection = document.querySelector('select');
-    let selectedMovie = selection.options[selection.selectedIndex].text;
+    let selection = document.getElementById("genres");
+    let selectedGenre = selection.options[selection.selectedIndex].text;
+    console.log(selectedGenre);
+    var url = "http://127.0.0.1:5000/getByGenre";
+    $.post(url, {
+        genre: selectedGenre,
+    },function(data, status) {
+        console.log(data, status);
+        var htmlContent="";
+        for(var i=0; i<data[0].genre_movies.length; i++) {
+            htmlContent = htmlContent + "<div class='col-6 col-sm-4 col-md-2 movie-card'><a href='/movie/'"+data[0].genre_movies[i]+"><p>"+data[0].genre_movies[i]+"</p></a></div>";
+        }
+        document.getElementById("genre-row").innerHTML = htmlContent;
+    });
+}
 
-    var url = "http://127.0.0.1:5000/movie/"+selectedMovie;
-    window.location = url;
+let selection = document.getElementById("years");
+
+
+function yearSelected() {
+    let selection = document.getElementById("years");
+    let selectedYear = selection.options[selection.selectedIndex].text;
+    var url = "http://127.0.0.1:5000/getByYear";
+    $.post(url, {
+        year: selectedYear,
+    },function(data, status) {
+        console.log(data, status);
+        var htmlContent="";
+        for(var i=0; i<data[0].year_movies.length; i++) {
+            htmlContent = htmlContent + "<div class='col-6 col-sm-4 col-md-2 movie-card'><a href='/movie/'"+data[0].year_movies[i]+"><p>"+data[0].year_movies[i]+"</p></a></div>";
+        }
+        document.getElementById("year-row").innerHTML = htmlContent;
+    });
 }
