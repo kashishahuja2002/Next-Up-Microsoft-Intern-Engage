ff679d6236b854dcf685dc089112956d078d71ad
Kashish Ahuja <kashish.ahuja-IT-2K19-24@iips.edu.in>
2022-05-27 23:38:37 +0530

    Updated database url

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('DATABSE_URL')
+conn = sqlite3.connect("postgres://uqtpxryhwlcazx:c0b39d30a1b2f9415b7aa7777277f7ecd9ad495c0859296ea63cdd1823b1d197@ec2-34-231-221-151.compute-1.amazonaws.com:5432/dc18lg9t4rf23o")
 cursor = conn.cursor()
 sql_query = """ Create table if not exists users (
     id integer primary key AUTOINCREMENT NOT NULL,
