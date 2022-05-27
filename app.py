2005598bb6cb4c8c12c9b06c5e11b273174b4271
Kashish Ahuja <kashish.ahuja-IT-2K19-24@iips.edu.in>
2022-05-28 00:26:13 +0530

    Removed CORS error

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ import operator
 from annoy import AnnoyIndex
 from flask_mail import Mail, Message
 from random import randint
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
 app.secret_key = "secret key"
+CORS(app, support_credentials=True)
 
 
 # Flask mail configuration
