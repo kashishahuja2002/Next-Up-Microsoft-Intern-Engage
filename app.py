816592769d16bfb1ea705e35faa971be5f3f0e4f
Kashish Ahuja <kashish.ahuja-IT-2K19-24@iips.edu.in>
2022-05-29 01:51:06 +0530

    Removed session on load

--- app.py
+++ app.py
@@ -32,10 +32,10 @@ conn.close()
 
 
 # Clear session
-if "user" in session:
-    session.pop("user", None)
-if "choices" in session:
-    session.pop("choices", None)
+# if "user" in session:
+#     session.pop("user", None)
+# if "choices" in session:
+#     session.pop("choices", None)
 
 
 # Home page
